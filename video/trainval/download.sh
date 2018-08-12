988b625e65f388b3950c8c9172b0dba7478c5e13
SunDoge <384813529@qq.com>
2018-08-12 15:14:37 +0800

    use wget instead

--- video/trainval/download.sh
+++ video/trainval/download.sh
@@ -3,5 +3,6 @@ ava_url="http://s3.gayhub.cn/ava-dataset/trainval/"
 while read p; do
   echo $p
   echo $ava_url$p -O;
-  curl $ava_url$p -O;
+  # curl $ava_url$p -O;
+  wget $ava_url$p
 done <ava_file_names_trainval_v2.1.txt
